65cbc4bfbc932899ccaf4995c372a64c5640c21e
AmeerTmsy <90926079+AmeerTmsy@users.noreply.github.com>
2025-09-11 10:06:09 +0530

    added 'Continue with Google' button to the login page

--- client/src/routes/login.jsx
+++ client/src/routes/login.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { authenticate } from '../features/login/loginSlice';
+import googleIcon from '../assets/googleIcon.png'
 
 function Login(props) {
     const [email, setEmail] = useState('');
@@ -75,7 +76,7 @@ function Login(props) {
         if (!validateForm()) return;
 
         const payload = { "email": email, "password": password }
-        console.log("URL: ",import.meta.env.VITE_API_URL)
+        console.log("URL: ", import.meta.env.VITE_API_URL)
         try {
             let url = `${import.meta.env.VITE_API_URL}/auth/login`;
             console.log(url);
@@ -101,7 +102,7 @@ function Login(props) {
     return (
         <>
             <div className='loginDiv_1'>
-                <form className='loginForm' onSubmit={handleSubmit} noValidate>
+                <div className='loginForm'>
                     <div className='formSec formSec1'>
                     </div>
                     <div className='formSec formSec2'>
@@ -113,48 +114,62 @@ function Login(props) {
                             />
                         </div>
                         <h1 className='loginH1'>Login</h1>
-                        <div className='passwordFieldWrap'>
-                            <div className='passDiv'>
-                                <input
-                                    className='py-2 px-5'
-                                    type="email"
-                                    name="email"
-                                    id="email"
-                                    placeholder='Email'
-                                    value={email}
-                                    onChange={(e) => setEmail(e.target.value)}
-                                />
+                        <form onSubmit={handleSubmit} noValidate>
+                            <div className='passwordFieldWrap'>
+                                <div className='passDiv'>
+                                    <input
+                                        className='py-2 px-5'
+                                        type="email"
+                                        name="email"
+                                        id="email"
+                                        placeholder='Email'
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                    />
+                                </div>
+                                {errors.email && <p className="error">{errors.email}</p>}
                             </div>
-                            {errors.email && <p className="error">{errors.email}</p>}
-                        </div>
-                        <div className='passwordFieldWrap'>
-                            <div className='passDiv'>
-                                <input
-                                    className='py-2 px-5'
-                                    type={passEyeSee ? 'text' : 'password'}
-                                    name="password"
-                                    id="password"
-                                    placeholder='Password'
-                                    value={password}
-                                    onChange={(e) => setPassword(e.target.value)}
-                                />
+                            <div className='passwordFieldWrap'>
+                                <div className='passDiv'>
+                                    <input
+                                        className='py-2 px-5'
+                                        type={passEyeSee ? 'text' : 'password'}
+                                        name="password"
+                                        id="password"
+                                        placeholder='Password'
+                                        value={password}
+                                        onChange={(e) => setPassword(e.target.value)}
+                                    />
 
-                                {
-                                    passEyeSee ?
-                                        (<i onClick={() => setPassEyeSee(false)} className="ri-eye-line pr-1"></i>)
-                                        :
-                                        (<i onClick={() => setPassEyeSee(true)} className="ri-eye-off-line pr-1"></i>)
-                                }
+                                    {
+                                        passEyeSee ?
+                                            (<i onClick={() => setPassEyeSee(false)} className="ri-eye-line pr-1"></i>)
+                                            :
+                                            (<i onClick={() => setPassEyeSee(true)} className="ri-eye-off-line pr-1"></i>)
+                                    }
 
+                                </div>
+                                {errors.password && <p className="error">{errors.password}</p>}
+                                <Link className='forgotLink' to={'/forgotPassword'}>forgot password?</Link>
                             </div>
-                            {errors.password && <p className="error">{errors.password}</p>}
-                            <Link className='forgotLink' to={'/forgotPassword'}>forgot password?</Link>
-                        </div>
-                        <div className=''>
-                            <button type="submit" className='subBtn'>Submit</button>
+                            <div className='passwordFieldWrap'>
+                                <button type="submit" className='subBtn'>Submit</button>
+                            </div>
+                        </form>
+                        <div className='passwordFieldWrap'>
+                            <div style={{ display: 'flex', justifyContent: 'center' }}>
+                                <button
+                                    style={{ display: 'flex', justifyContent: 'center', gap: '1em', padding: '0.5em 1em', width: '100%' }}
+                                    onClick={() => {
+                                        window.location.href = "http://localhost:3000/api/auth/google";
+                                    }}
+                                >
+                                    <img src={googleIcon} alt="google icon" />Continue with Google
+                                </button>
+                            </div>
                         </div>
                     </div>
-                </form>
+                </div>
             </div>
         </>
     );
